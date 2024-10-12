d5e628e357b576c3b8d77696db9480e81d5402fc
kevinkits2 <kevin.kits@gmail.com>
2024-10-12 13:37:20 +0300

    added sprites

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -5,8 +5,15 @@ using UnityEngine.AI;
 
 public class Pathfinder : MonoBehaviour {
 
+    private const string IS_MOVING = "IsMoving";
+
     [SerializeField] private Transform finish;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
     private NavMeshAgent agent;
+    private bool isMoving = false;
+    private bool finished = false;
 
 
     private void Awake() {
@@ -20,8 +27,33 @@ public class Pathfinder : MonoBehaviour {
     }
 
     private void HandleGameStateChanged(GameState state) {
-        if (state == GameState.PlayMode) {
+        if (state == GameState.PlayMode && !finished) {
             agent.destination = finish.position;
+            isMoving = true;
+        }
+    }
+
+    private void Update() {
+        if (finished) return;
+
+        FlipSprite();
+        DestinationCheck();
+        animator.SetBool(IS_MOVING, isMoving);
+    }
+
+    private void DestinationCheck() {
+        if (Vector3.Distance(transform.position, finish.position) < 0.1f) {
+            isMoving = false;
+            finished = true;
+        }
+    }
+
+    private void FlipSprite() {
+        if (agent.velocity.x > 0) {
+            spriteRenderer.flipX = false;
+        }
+        else {
+            spriteRenderer.flipX = true;
         }
     }
 }
